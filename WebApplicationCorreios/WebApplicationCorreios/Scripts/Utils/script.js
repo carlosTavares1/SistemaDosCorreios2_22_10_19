df0146888d13a20d5061d983d20523a1963c8125
Carlos Tavares <cast.3@hotmail.com>
2019-10-24 15:31:09 -0300

    changes

--- WebApplicationCorreios/WebApplicationCorreios/Scripts/Utils/script.js
+++ WebApplicationCorreios/WebApplicationCorreios/Scripts/Utils/script.js
@@ -1,34 +1,53 @@
-﻿$(document).ready(
+﻿var obj = {
+    nCdEmpresa: "",
+    sDsSenha: "",
+    sCepOrigem: "",
+    sCepDestino: "",
+    nVIPeso: "",
+    nCdFormato: "",
+    nVIComprimento: "",
+    nVIAltura: "",
+    nVILargura: "",
+    sCdMaoPropria: "",
+    nVIValorDeclarado: "",
+    nCdAvisoRecebimento: "",
+    nCdServico: "",
+    nVIDiametro: "",
+    StrRetorno: "",
+    nIndicaCalculo: ""
+}
+$(document).ready(
     $('input[name="btn"]').click(
         function () {
-            $.getJSON("http://usysweb.com.br/api/correiosambev.php?nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=70002900&sCepDestino=04547000&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=20&nVlLargura=20&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3", function (data) {
-                var obj = {
-                    nCdEmpresa: "",
-                    sDsSenha: "",
-                    sCepOrigem: $('input[name="cepori"]').val(),
-                    sCepDestino: $('input[name="cepdes"]').val(),
-                    nVIPeso: $('input[name="peso"]').val(),
-                    nCdFormato: $('input[name="formato"]').val(),
-                    nVIComprimento: $('input[name="comprimento"]').val(),
-                    nVIAltura: $('input[name="altura"]').val(),
-                    nVILargura: $('input[name="largura"]').val(),
-                    sCdMaoPropria: $('input[name="maopropria"]').val(),
-                    nVIValorDeclarado: $('input[name="valordeclarado"]').val(),
-                    nCdAvisoRecebimento: $('input[name="avisorecebimento"]').val(),
-                    nCdServico: $('input[name="servicos"]').val(),
-                    nVIDiametro: $('input[name="diametro"]').val(),
-                }
 
-                $.post("http://usysweb.com.br/api/correiosambev.php?giomar=true", obj, function (data) {
-                    var ObjetoCEP = JSON.stringify(obj);/*Convertendo o objeto para json*/
-                    //window.location.replace("Consulta.html");
-                    
-                    //window.location = "Consulta.html?obj=" + ObjetoCEP;
-                    alert(ObjetoCEP)
-                    //$("input").append(ObjetoCEP);
+            obj.nCdEmpresa = $('input[name="codigo"]').val(),
+                obj.sDsSenha = $('input[name="senha"]').val(),
+                obj.sCepOrigem = $('input[name="cepori"]').val(),
+                obj.sCepDestino = $('input[name="cepdes"]').val(),
+                obj.nVIPeso = $('input[name="peso"]').val(),
+                obj.nCdFormato = $('input[name="formato"]').val(),
+                obj.nVIComprimento = $('input[name="comprimento"]').val(),
+                obj.nVIAltura = $('input[name="altura"]').val(),
+                obj.nVILargura = $('input[name="largura"]').val(),
+                obj.sCdMaoPropria = $('input[name="maopropria"]').val(),
+                obj.nVIValorDeclarado = $('input[name="valordeclarado"]').val(),
+                obj.nCdAvisoRecebimento = $('input[name="avisorecebimento"]').val(),
+                obj.nCdServico = $('input[name="servicos"]').val(),
+                obj.nVIDiametro = $('input[name="diametro"]').val(),
+                obj.StrRetorno = "xml",
+                obj.nIndicaCalculo = 3
 
-                });
+
+            $.getJSON("http://usysweb.com.br/api/correiosambev.php?",obj, function (data) {
+                //var ObjetoCEP = JSON.stringify(obj);/*Convertendo o objeto para json*/
+                //window.location.replace("Consulta.html");
+
+                //window.location = "Consulta.html?obj=" + ObjetoCEP;
+                alert(data.cServico);
+                //$("input").append(ObjetoCEP);
+
             });
+
 
             /*
    submit JSON as 'post' to a new page
