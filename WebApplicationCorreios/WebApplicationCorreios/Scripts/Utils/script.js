b4523c6a785e5b19891d74d7dba48d6eb0223ecf
ABHU Brahaitianos <abhublumenau6@gmail.com>
2019-10-25 16:55:36 -0300

    yy

--- WebApplicationCorreios/WebApplicationCorreios/Scripts/Utils/script.js
+++ WebApplicationCorreios/WebApplicationCorreios/Scripts/Utils/script.js
@@ -1,68 +1,23 @@
-﻿var obj = {
-    nCdEmpresa: "",
-    sDsSenha: "",
-    sCepOrigem: "",
-    sCepDestino: "",
-    nVIPeso: "",
-    nCdFormato: "",
-    nVIComprimento: "",
-    nVIAltura: "",
-    nVILargura: "",
-    sCdMaoPropria: "",
-    nVIValorDeclarado: "",
-    nCdAvisoRecebimento: "",
-    nCdServico: "",
-    nVIDiametro: "",
-    StrRetorno: "",
-    nIndicaCalculo: ""
-}
-$(document).ready(
-    $('input[name="btn"]').click(
-        function () {
+﻿
 
-            obj.nCdEmpresa = $('input[name="codigo"]').val(),
-                obj.sDsSenha = $('input[name="senha"]').val(),
-                obj.sCepOrigem = $('input[name="cepori"]').val(),
-                obj.sCepDestino = $('input[name="cepdes"]').val(),
-                obj.nVIPeso = $('input[name="peso"]').val(),
-                obj.nCdFormato = $('input[name="formato"]').val(),
-                obj.nVIComprimento = $('input[name="comprimento"]').val(),
-                obj.nVIAltura = $('input[name="altura"]').val(),
-                obj.nVILargura = $('input[name="largura"]').val(),
-                obj.sCdMaoPropria = $('input[name="maopropria"]').val(),
-                obj.nVIValorDeclarado = $('input[name="valordeclarado"]').val(),
-                obj.nCdAvisoRecebimento = $('input[name="avisorecebimento"]').val(),
-                obj.nCdServico = $('input[name="servicos"]').val(),
-                obj.nVIDiametro = $('input[name="diametro"]').val(),
-                obj.StrRetorno = "xml",
-                obj.nIndicaCalculo = 3
+$(document).ready(function () {
+    $('input[name="bntconsultar"]').click(function () {
+        //obter as informações dos meus campos
+        //Obtemos as informações do nosso formulario web completo
+        var informacoes = $('form[name="formenviar"]').serializeArray();
+        //Obtemos o atributo que indica a pagina que vamos enviar o post
+        var obterAtributo = $('form[name="formenviar"]').attr('send-post');
 
-
-            $.getJSON("http://usysweb.com.br/api/correiosambev.php?",obj, function (data) {
-                //var ObjetoCEP = JSON.stringify(obj);/*Convertendo o objeto para json*/
-                //window.location.replace("Consulta.html");
-
-                //window.location = "Consulta.html?obj=" + ObjetoCEP;
-                alert(data.cServico);
-                //$("input").append(ObjetoCEP);
-
+        //Enviamos o post para nosso servidor web
+                                     //api/CalculoImc?altura={altura}&peso={peso}&nomeUsuario={nomeUsuario}
+        $.get("http://localhost:52242/Api/CalculoImc?altura=1.89&peso=70&nomeUsuario=web", function (data) {
+  
+            //temos que passar ela para o JSON parecido com o int.Parse()
+            data = JSON.parse(data);
+            //Aqui carregamos as informações automaticamente
+            $.each(data.cServico, function (key, value) {
+                $('p[name="{key}"'.replace("{key}", key)).text(value);
             });
-
-
-            /*
-   submit JSON as 'post' to a new page
-   Parameters:
-   path        (URL)   path to the new page
-   data        (obj)   object to be converted to JSON and passed
-   postName    (str)   name of the POST parameter to send the JSON
-*/
-
-
-                }
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                $('#modal-fracasso').modal('show')
-                $('#spinner').css("display", "none")
-            })
-    })
-})
+        });
+    });
+});
